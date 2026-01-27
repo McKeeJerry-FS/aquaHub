abb28db8ab9ccb170300da254377a3bfb29afd7b
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-01-27 09:08:16 -0500

    Updated the Record Water Test view

--- AquaHub/obj/Debug/net10.0/AquaHub.AssemblyInfo.cs
+++ AquaHub/obj/Debug/net10.0/AquaHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3745ba34a2765f1a817f195cdf7a9bed032c6df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b235b47a5f21152488471504285cc6162b7107b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
