34da2b448f25a52b4985eb5ee88fc196ad99bfd3
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-01-29 09:52:22 -0500

    Updated the app landing page for new and upcoming features

--- AquaHub/obj/Debug/net10.0/AquaHub.AssemblyInfo.cs
+++ AquaHub/obj/Debug/net10.0/AquaHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4a7dd0707dc6bf0f274596347397de5627b9e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c61f0a5d43b442715755713764ca8603ea08491")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
